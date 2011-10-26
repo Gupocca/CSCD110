e83a2eb91656167dc964d854a9d333195620cf5b
Stephen Hoerner <legolas710@gmail.com>
2011-10-26 06:30:27 -0700

    Template for statistics engine

---
+++ statistics.py
@@ -0,0 +1,31 @@
+def main():
+    pass
+
+def showMenu():
+    print('1. add number')
+    
+    pass
+
+def addNum():
+    pass
+
+def delNum():
+    pass
+
+def purgeList():
+    pass
+
+def showList():
+    pass
+
+def getMean():
+    pass
+
+def getMidRange():
+    pass
+
+def getMedian():
+    pass
+
+def getMode():
+    pass
